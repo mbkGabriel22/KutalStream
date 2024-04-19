23ab1752a84908c01fcb755863d4edd983cd113a
mbkGabriel22 <105675929+mbkGabriel22@users.noreply.github.com>
2024-04-19 23:57:55 +0100

    presentations/bloc/popular_movies

---
+++ lib/presentation/bloc/popular_movies/popular_movies_state.dart
@@ -0,0 +1,19 @@
+import 'package:kutal_stream_app/domain/entities/Movie.dart';
+
+abstract class PopularMoviesState {}
+
+class PopularMoviesInitial extends PopularMoviesState {}
+
+class PopularMoviesLoading extends PopularMoviesState {}
+
+class PopularMoviesLoaded extends PopularMoviesState {
+  final List<Movie> movies;
+
+  PopularMoviesLoaded(this.movies);
+}
+
+class PopularMoviesError extends PopularMoviesState {
+  final String message;
+
+  PopularMoviesError(this.message);
+}
