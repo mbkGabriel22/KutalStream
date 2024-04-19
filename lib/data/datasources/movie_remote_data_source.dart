404d1f3f689b33c873aae9a3aab1e385efd289e4
mbkGabriel22 <105675929+mbkGabriel22@users.noreply.github.com>
2024-04-19 23:06:52 +0100

    data/finished

---
+++ lib/data/datasources/movie_remote_data_source.dart
@@ -0,0 +1,9 @@
+import 'package:kutal_stream_app/data/models/movie_model.dart';
+
+abstract class MovieRemoteDataSource {
+  Future<List<MovieModel>> getTrendingMovies();
+
+  Future<List<MovieModel>> searchMovies(String query);
+
+  Future<List<MovieModel>> getPopularMovies();
+}
---
+++ lib/data/datasources/remote/movie_remote_data_source_impl.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:kutal_stream_app/core/errors/server_exception.dart';
+import 'package:kutal_stream_app/data/datasources/movie_remote_data_source.dart';
+import 'package:kutal_stream_app/data/models/movie_model.dart';
+
+class MovieRemoteDataSourceImpl implements MovieRemoteDataSource {
+  final http.Client client;
+
+  MovieRemoteDataSourceImpl({required this.client});
+
+  static const BASE_URL = "https://api.themoviedb.org/3";
+  static const API_KEY = "c3e653dff4bd93a2b4ae4b3a6d8c1912";
+
+  @override
+  Future<List<MovieModel>> getPopularMovies() async {
+    final response = await client.get(
+      Uri.parse("$BASE_URL/movie/popular?api_key=$API_KEY"),
+    );
+
+    if (response.statusCode == 200) {
+      final responseBody = json.decode(response.body);
+      final List<MovieModel> movies = (responseBody['results'] as List)
+          .map((movie) => MovieModel.fromJson(movie))
+          .toList();
+      return movies;
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<List<MovieModel>> getTrendingMovies() async {
+    final response = await client.get(
+      Uri.parse("$BASE_URL/trending/movie/day?api_key=$API_KEY"),
+    );
+
+    if (response.statusCode == 200) {
+      final responseBody = json.decode(response.body);
+      final List<MovieModel> movies = (responseBody['results'] as List)
+          .map((movie) => MovieModel.fromJson(movie))
+          .toList();
+      return movies;
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<List<MovieModel>> searchMovies(String query) async {
+    final response = await client.get(
+      Uri.parse("$BASE_URL/search/movie?query=$query&api_key=$API_KEY"),
+    );
+
+    if (response.statusCode == 200) {
+      final responseBody = json.decode(response.body);
+      final List<MovieModel> movies = (responseBody['results'] as List)
+          .map((movie) => MovieModel.fromJson(movie))
+          .toList();
+      return movies;
+    } else {
+      throw ServerException();
+    }
+  }
+}
