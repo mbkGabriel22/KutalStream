631ca9615ab5c4ca2fcafa0c57087bd7274a2427
mbkGabriel22 <105675929+mbkGabriel22@users.noreply.github.com>
2024-04-19 22:57:10 +0100

    Data/models

--- lib/domain/entities/Movie.dart
+++ lib/domain/entities/Movie.dart
@@ -1,4 +1,6 @@
-class Movie {
+import 'package:equatable/equatable.dart';
+
+class Movie extends Equatable {
   final int id;
   final String title;
   final String overview;
@@ -9,4 +11,7 @@ class Movie {
       required this.title,
       required this.overview,
       required this.posterPath});
+
+  @override
+  List<Object?> get props => [id, title, overview, posterPath];
 }
