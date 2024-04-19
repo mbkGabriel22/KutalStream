a3e952175ad46448dbe277f2975c16fc0fef364d
mbkGabriel22 <105675929+mbkGabriel22@users.noreply.github.com>
2024-04-19 23:14:33 +0100

    data/repositories finished

---
+++ lib/data/repositories/movie_repository_impl.dart
@@ -0,0 +1,51 @@
+import 'package:dartz/dartz.dart';
+import 'package:kutal_stream_app/core/errors/server_failure.dart';
+import 'package:kutal_stream_app/data/datasources/movie_remote_data_source.dart';
+import 'package:kutal_stream_app/data/models/movie_model.dart';
+import 'package:kutal_stream_app/domain/entities/Movie.dart';
+import 'package:kutal_stream_app/domain/repositories/movie_repository.dart';
+
+class MovieRepositoryImpl implements MovieRepository {
+  final MovieRemoteDataSource remoteDataSource;
+
+  MovieRepositoryImpl({required this.remoteDataSource});
+
+  @override
+  Future<Either<ServerFailure, List<Movie>>> getPopularMovies() async {
+    try {
+      final List<MovieModel> movieModels =
+          await remoteDataSource.getPopularMovies();
+      final List<Movie> movies =
+          movieModels.map((model) => model.toEntity()).toList();
+      return Right(movies);
+    } catch (e) {
+      return Left(ServerFailure());
+    }
+  }
+
+  @override
+  Future<Either<ServerFailure, List<Movie>>> getTrendingMovies() async {
+    try {
+      final List<MovieModel> movieModels =
+          await remoteDataSource.getTrendingMovies();
+      final List<Movie> movies =
+          movieModels.map((model) => model.toEntity()).toList();
+      return Right(movies);
+    } catch (e) {
+      return Left(ServerFailure());
+    }
+  }
+
+  @override
+  Future<Either<ServerFailure, List<Movie>>> searchMovies(String query) async {
+    try {
+      final List<MovieModel> movieModels =
+          await remoteDataSource.searchMovies(query);
+      final List<Movie> movies =
+          movieModels.map((model) => model.toEntity()).toList();
+      return Right(movies);
+    } catch (e) {
+      return Left(ServerFailure());
+    }
+  }
+}
