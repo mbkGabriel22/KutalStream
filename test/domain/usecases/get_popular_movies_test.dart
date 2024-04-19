6b93f827833a8337870a3b9985adf4ea21f3a870
mbkGabriel22 <105675929+mbkGabriel22@users.noreply.github.com>
2024-04-19 23:27:35 +0100

    test/domain

--- test/domain/usecases/get_popular_movies_test.dart
+++ test/domain/usecases/get_popular_movies_test.dart
@@ -1,21 +1,22 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:kutal_stream_app/domain/entities/Movie.dart';
-import 'package:kutal_stream_app/domain/usecases/search_movie.dart';
+import 'package:kutal_stream_app/domain/repositories/movie_repository.dart';
+import 'package:kutal_stream_app/domain/usecases/get_popular_movies.dart';
 
 import 'get_popular_movies_test.mocks.dart';
 
+@GenerateNiceMocks([MockSpec<MovieRepository>()])
 void main() {
-  late SearchMovies usecase;
+  late GetPopularMovies usecase;
   late MockMovieRepository mockMovieRepository;
 
   setUp(() {
     mockMovieRepository = MockMovieRepository();
-    usecase = SearchMovies(mockMovieRepository);
+    usecase = GetPopularMovies(mockMovieRepository);
   });
-
-  final String tQuery = 'Inception';
 
   final tMoviesList = [
     Movie(
@@ -30,15 +31,15 @@ void main() {
         posterPath: "/image2"),
   ];
 
-  test('should get movies from the query from the repository', () async {
+  test('should get popular movies from the repository', () async {
     // arrange
-    when(mockMovieRepository.searchMovies(any))
+    when(mockMovieRepository.getPopularMovies())
         .thenAnswer((_) async => Right(tMoviesList));
     // act
-    final result = await usecase(tQuery);
+    final result = await usecase();
     // assert
     expect(result, equals(Right(tMoviesList)));
-    verify(mockMovieRepository.searchMovies(tQuery));
+    verify(mockMovieRepository.getPopularMovies());
     verifyNoMoreInteractions(mockMovieRepository);
   });
 }
