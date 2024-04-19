6f04a0d8dc9f4c0b44920e19ba1f0faa0be089e1
mbkGabriel22 <105675929+mbkGabriel22@users.noreply.github.com>
2024-04-20 00:19:38 +0100

    presntation finished

---
+++ lib/presentation/injection_contenair.dart
@@ -0,0 +1,37 @@
+import 'package:get_it/get_it.dart';
+import 'package:kutal_stream_app/data/datasources/movie_remote_data_source.dart';
+import 'package:kutal_stream_app/data/datasources/remote/movie_remote_data_source_impl.dart';
+import 'package:kutal_stream_app/data/repositories/movie_repository_impl.dart';
+import 'package:kutal_stream_app/domain/repositories/movie_repository.dart';
+import 'package:kutal_stream_app/domain/usecases/get_popular_movies.dart';
+import 'package:kutal_stream_app/domain/usecases/get_trending_movies.dart';
+import 'package:kutal_stream_app/domain/usecases/search_movie.dart';
+import 'package:kutal_stream_app/presentation/bloc/popular_movies/popular_movies_bloc.dart';
+import 'package:kutal_stream_app/presentation/bloc/search_movies/search_movies_bloc.dart';
+import 'package:kutal_stream_app/presentation/bloc/trending_movies/trending_movies_bloc.dart';
+import 'package:http/http.dart' as http;
+
+final getIt = GetIt.instance;
+
+void init() {
+  // Bloc
+  getIt.registerFactory(() => PopularMoviesBloc(getPopularMovies: getIt()));
+  getIt.registerFactory(() => TrendingMoviesBloc(getTrendingMovies: getIt()));
+  getIt.registerFactory(() => SearchMoviesBloc(searchMovies: getIt()));
+
+  // Use cases
+  getIt.registerLazySingleton(() => GetPopularMovies(getIt()));
+  getIt.registerLazySingleton(() => GetTrendingMovies(getIt()));
+  getIt.registerLazySingleton(() => SearchMovies(getIt()));
+
+  // Repositories
+  getIt.registerLazySingleton<MovieRepository>(
+      () => MovieRepositoryImpl(remoteDataSource: getIt()));
+
+  // Data sources
+  getIt.registerLazySingleton<MovieRemoteDataSource>(
+      () => MovieRemoteDataSourceImpl(client: getIt()));
+
+  // Http service
+  getIt.registerLazySingleton(() => http.Client());
+}
