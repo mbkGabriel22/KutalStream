e367a0e2b2fe7b7d980cb0d7d3cc38a9ce610784
mbkGabriel22 <105675929+mbkGabriel22@users.noreply.github.com>
2024-04-20 00:09:44 +0100

    presentation/bloc/search_movies

---
+++ lib/presentation/bloc/search_movies/search_movies_bloc.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kutal_stream_app/domain/usecases/search_movie.dart';
+import 'package:kutal_stream_app/presentation/bloc/search_movies/search_movies_event.dart';
+import 'package:kutal_stream_app/presentation/bloc/search_movies/search_movies_state.dart';
+
+class SearchMoviesBloc extends Bloc<SearchMoviesEvent, SearchMoviesState> {
+  final SearchMovies searchMovies;
+
+  SearchMoviesBloc({required this.searchMovies})
+      : super(SearchMoviesInitial()) {
+    on<FetchSearchMovies>((event, emit) async {
+      emit(SearchMoviesLoading());
+      final failureOrMovies = await searchMovies(event.query);
+      failureOrMovies.fold(
+          (failure) => emit(SearchMoviesError(failure.toString())),
+          (movies) => emit(SearchMoviesLoaded(movies)));
+    });
+  }
+}
---
+++ lib/presentation/bloc/search_movies/search_movies_state.dart
@@ -0,0 +1,19 @@
+import 'package:kutal_stream_app/domain/entities/Movie.dart';
+
+abstract class SearchMoviesState {}
+
+class SearchMoviesInitial extends SearchMoviesState {}
+
+class SearchMoviesLoading extends SearchMoviesState {}
+
+class SearchMoviesLoaded extends SearchMoviesState {
+  final List<Movie> movies;
+
+  SearchMoviesLoaded(this.movies);
+}
+
+class SearchMoviesError extends SearchMoviesState {
+  final String message;
+
+  SearchMoviesError(this.message);
+}
